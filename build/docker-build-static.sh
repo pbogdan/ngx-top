a7974c4a5ffd6fa59f2f16779576b939764f6d50
Piotr Bogdan <ppbogdan@gmail.com>
2018-03-25 21:27:37 +0100

    Fix static builds.

--- build/docker-build-static.sh
+++ build/docker-build-static.sh
@@ -11,11 +11,13 @@ apk add alpine-sdk \
     cabal stack \
     ncurses-dev \
     ncurses-static
+stack upgrade
+export PATH=$HOME/.local/bin:$PATH
 [ ! -f /usr/local/bin/upx ] && wget https://github.com/lalyos/docker-upx/releases/download/v3.91/upx \
      -O /usr/local/bin/upx && \
     chmod +x /usr/local/bin/upx
 cd /tmp/src/ngx-top && \
-    stack install --only-dependencies --system-ghc && \
-    stack --local-bin-path /build/static/ install --test --system-ghc \
-          --ghc-options '-optl-static' && \
+    stack install --only-dependencies --system-ghc --skip-ghc-check && \
+    stack --local-bin-path /build/static/ install --test --skip-ghc-check \
+    --system-ghc --ghc-options '-optl-static' && \
     upx -q /build/static/ngx-top
