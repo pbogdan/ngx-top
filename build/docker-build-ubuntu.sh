7e6628ef2bfb1c58533730a2b7d09f1c008d0e40
Piotr Bogdan <ppbogdan@gmail.com>
2019-12-09 09:42:33 +0000

    Limit Ubuntu builds to 16.04 & 18.04.

--- build/docker-build-ubuntu.sh
+++ build/docker-build-ubuntu.sh
@@ -3,7 +3,7 @@ set -e
 cp -r /src/ .
 rm -r **/*/.stack-work
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -y install python-software-properties software-properties-common
+DEBIAN_FRONTEND=noninteractive apt-get -y install software-properties-common
 add-apt-repository ppa:hvr/ghc
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y install git \
@@ -11,8 +11,8 @@ DEBIAN_FRONTEND=noninteractive apt-get -y install git \
     zlib1g-dev \
     libtinfo-dev \
     wget \
-    ghc-8.0.2 \
-    cabal-install-1.24
+    ghc-8.6.5 \
+    cabal-install-2.4
 [ ! -f /usr/local/bin/stack ] && wget -qO- https://get.haskellstack.org/ | sh
 [ ! -f /usr/local/bin/upx ] && wget https://github.com/lalyos/docker-upx/releases/download/v3.91/upx \
      -O /usr/local/bin/upx && \
